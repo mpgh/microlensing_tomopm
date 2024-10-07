2ccacea871642e042e21a8c192c8e386c4b6980c
mpgh <markus.hundertmark@uni-heidelberg.de>
2024-10-07 16:24:44 +0200

    change settings to prep for dockerfile

--- microlensing_tomopm/settings.py
+++ microlensing_tomopm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('opm_key')
+SECRET_KEY = os.getenv('OPM_KEY','test')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,6 +70,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
